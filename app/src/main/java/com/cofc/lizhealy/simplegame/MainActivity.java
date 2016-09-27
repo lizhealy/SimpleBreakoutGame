de1dfefcb466b7583a28f551b99e5fc8a29e89ae
lizhealy <lizhealy8@gmail.com>
2016-09-27 15:39:17 -0400

    Implemented paddle, ball, bricks

--- app/src/main/java/com/cofc/lizhealy/simplegame/MainActivity.java
+++ app/src/main/java/com/cofc/lizhealy/simplegame/MainActivity.java
@@ -1,15 +1,24 @@
 package com.cofc.lizhealy.simplegame;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.RectF;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,12 +42,73 @@ public class MainActivity extends AppCompatActivity {
         private long timeThisFrame;
         int screenX;
         int screenY;
-
+        Paddle paddle;
+        Ball ball;
+        Brick[] bricks = new Brick[200];
+        int numBricks = 0;
+        SoundPool soundPool;
+        int beep1ID = -1;
+        int beep2ID = -1;
+        int beep3ID = -1;
+        int loseLifeID = -1;
+        int explodeID = -1;
+        int score = 0;
+        int lives = 3;
 
         public BreakoutView(Context context) {
             super(context);
             ourHolder = getHolder();
             paint = new Paint();
+            Display display = getWindowManager().getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenX = size.x;
+            screenY = size.y;
+
+            paddle = new Paddle(screenX, screenY);
+            ball = new Ball(screenX, screenY);
+
+            soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+            try {
+                AssetManager assetManager = context.getAssets();
+                AssetFileDescriptor descriptor;
+
+                descriptor = assetManager.openFd("beep1.ogg");
+                beep1ID = soundPool.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("beep2.ogg");
+                beep2ID = soundPool.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("beep3.ogg");
+                beep3ID = soundPool.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("loseLife.ogg");
+                loseLifeID = soundPool.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("explode.ogg");
+                explodeID = soundPool.load(descriptor, 0);
+
+            } catch (IOException e) {
+                Log.e("error", "failed to load sound files");
+            }
+
+            createBricksAndRestart();
+        }
+        public void createBricksAndRestart() {
+            ball.reset(screenX, screenY);
+            int brickWidth = screenX / 8;
+            int brickHeight = screenY / 10;
+            numBricks = 0;
+            for (int column = 0; column < 8; column++) {
+                for (int row = 0; row < 3; row++) {
+                    bricks[numBricks] = new Brick(row, column, brickWidth, brickHeight);
+                    numBricks++;
+                }
+            }
+            if (lives == 0) {
+                score = 0;
+                lives = 3;
+            }
         }
         @Override
         public void run() {
@@ -55,13 +125,125 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         public void update() {
+            paddle.update(fps);
+            ball.update(fps);
 
+            // Check for ball colliding with a brick
+            for (int i = 0; i < numBricks; i++) {
+                if (bricks[i].getVisibility()) {
+                    if (RectF.intersects(bricks[i].getRect(), ball.getRect())) {
+                        bricks[i].setInvisible();
+                        ball.reverseYVelocity();
+                        score = score + 10;
+                        soundPool.play(explodeID, 1, 1, 0, 0, 1);
+                    }
+                }
+            }
+            // Check for ball colliding with paddle
+            if (RectF.intersects(paddle.getRect(), ball.getRect())) {
+                ball.setRandomXVelocity();
+                ball.reverseYVelocity();
+                ball.clearObstacleY(paddle.getRect().top - 2);
+                soundPool.play(beep1ID, 1, 1, 0, 0, 1);
+            }
+            // Bounce the ball back when it hits the bottom of screen
+            if (ball.getRect().bottom > screenY) {
+                ball.reverseYVelocity();
+                ball.clearObstacleY(screenY - 2);
+
+                // Lose a life
+                lives--;
+                soundPool.play(loseLifeID, 1, 1, 0, 0, 1);
+
+                if (lives == 0) {
+                    paused = true;
+                    createBricksAndRestart();
+                }
+            }
+
+            // Bounce the ball back when it hits the top of screen
+            if (ball.getRect().top < 0)
+
+            {
+                ball.reverseYVelocity();
+                ball.clearObstacleY(12);
+
+                soundPool.play(beep2ID, 1, 1, 0, 0, 1);
+            }
+
+            // If the ball hits left wall bounce
+            if (ball.getRect().left < 0)
+
+            {
+                ball.reverseXVelocity();
+                ball.clearObstacleX(2);
+                soundPool.play(beep3ID, 1, 1, 0, 0, 1);
+            }
+
+            // If the ball hits right wall bounce
+            if (ball.getRect().right > screenX - 10) {
+
+                ball.reverseXVelocity();
+                ball.clearObstacleX(screenX - 22);
+
+                soundPool.play(beep3ID, 1, 1, 0, 0, 1);
+            }
+
+            // Pause if cleared screen
+            if (score == numBricks * 10)
+
+            {
+                paused = true;
+                createBricksAndRestart();
+            }
         }
         public void draw() {
-            if(ourHolder.getSurface().isValid()) {
+            if (ourHolder.getSurface().isValid()) {
+                // Lock the canvas ready to draw
                 canvas = ourHolder.lockCanvas();
+
+                // Draw the background color
                 canvas.drawColor(Color.argb(255, 26, 128, 182));
+
+                // Choose the brush color for drawing
                 paint.setColor(Color.argb(255, 255, 255, 255));
+
+                // Draw the paddle
+                canvas.drawRect(paddle.getRect(), paint);
+
+                // Draw the ball
+                canvas.drawRect(ball.getRect(), paint);
+
+                // Change the brush color for drawing
+                paint.setColor(Color.argb(255, 249, 129, 0));
+
+                // Draw the bricks if visible
+                for (int i = 0; i < numBricks; i++) {
+                    if (bricks[i].getVisibility()) {
+                        canvas.drawRect(bricks[i].getRect(), paint);
+                    }
+                }
+
+                // Choose the brush color for drawing
+                paint.setColor(Color.argb(255, 255, 255, 255));
+
+                // Draw the score
+                paint.setTextSize(40);
+                canvas.drawText("Score: " + score + "   Lives: " + lives, 10, 50, paint);
+
+                // Has the player cleared the screen?
+                if (score == numBricks * 10) {
+                    paint.setTextSize(90);
+                    canvas.drawText("YOU HAVE WON!", 10, screenY / 2, paint);
+                }
+
+                // Has the player lost?
+                if (lives <= 0) {
+                    paint.setTextSize(90);
+                    canvas.drawText("YOU HAVE LOST!", 10, screenY / 2, paint);
+                }
+
+                // Draw everything to the screen
                 ourHolder.unlockCanvasAndPost(canvas);
             }
         }
@@ -81,9 +263,21 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onTouchEvent(MotionEvent motionEvent) {
             switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+                // Player has touched the screen
                 case MotionEvent.ACTION_DOWN:
+                    paused = false;
+                    if (motionEvent.getX() > screenX / 2) {
+
+                        paddle.setMovementState(paddle.RIGHT);
+                    } else
+
+                    {
+                        paddle.setMovementState(paddle.LEFT);
+                    }
                     break;
+                // Player has removed finger from screen
                 case MotionEvent.ACTION_UP:
+                    paddle.setMovementState(paddle.STOPPED);
                     break;
             }
             return true;
