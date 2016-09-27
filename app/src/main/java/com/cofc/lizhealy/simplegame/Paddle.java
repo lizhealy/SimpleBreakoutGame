1a3949ab54d5be30b306af05f519ef6ec98055c0
lizhealy <lizhealy8@gmail.com>
2016-09-27 15:18:09 -0400

    Added Paddle Class

--- app/src/main/java/com/cofc/lizhealy/simplegame/Paddle.java
+++ app/src/main/java/com/cofc/lizhealy/simplegame/Paddle.java
@@ -1,7 +1,42 @@
 package com.cofc.lizhealy.simplegame;
 
-/**
- * Created by lizhealy on 9/27/16.
- */
+import android.graphics.RectF;
+
 public class Paddle {
+    private RectF rect;
+    private float length;
+    private float height;
+    private float x;
+    private float y;
+    private float paddleSpeed;
+    public final int STOPPED = 0;
+    public final int LEFT = 1;
+    public final int RIGHT = 2;
+    private int paddleMoving = STOPPED;
+
+    public Paddle(int screenX, int screenY) {
+        length = 130;
+        height = 20;
+        x = screenX / 2;
+        y = screenY - 20;
+        rect = new RectF(x,y,x+length,y+height);
+        paddleSpeed = 350;
+    }
+    public RectF getRect() {
+        return rect;
+    }
+    public void setMovementState(int state) {
+        paddleMoving = state;
+    }
+    public void update(long fps) {
+        if(paddleMoving == LEFT) {
+            x = x - paddleSpeed / fps;
+        }
+        if(paddleMoving == RIGHT) {
+            x = x + paddleSpeed / fps;
+        }
+        rect.left = x;
+        rect.right = x + length;
+    }
+
 }
